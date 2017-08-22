249e5577b386536121a856a8fb1e8f0d88e88667
Alexey Nabrodov <alexey.nabrodov@team.wrike.com>
2017-08-22 20:42:31 +0300

    add imgui

---
+++ Makefile
@@ -0,0 +1,52 @@
+#
+# Cross Platform Makefile
+# Compatible with MSYS2/MINGW, Ubuntu 14.04.1 and Mac OS X
+#
+#
+# You will need SDL2 (http://www.libsdl.org)
+#
+#   apt-get install libsdl2-dev  # Linux
+#   brew install sdl2            # Mac OS X
+#   pacman -S mingw-w64-i686-SDL # MSYS2
+#
+
+#CXX = g++
+
+EXE = mapgen
+OBJS = main.o imgui_impl_sdl_gl3.o
+OBJS += imgui/imgui.o imgui/imgui_demo.o imgui/imgui_draw.o
+OBJS += imgui/examples/libs/gl3w/GL/gl3w.o
+
+UNAME_S := $(shell uname -s)
+
+
+ifeq ($(UNAME_S), Linux) #LINUX
+	ECHO_MESSAGE = "Linux"
+	LIBS = -lGL -ldl `sdl2-config --libs`
+
+	CXXFLAGS = -Iimgui -Iimgui/examples/libs/gl3w `sdl2-config --cflags`
+	CXXFLAGS += -Wall -Wformat
+	CFLAGS = $(CXXFLAGS)
+endif
+
+ifeq ($(findstring MINGW,$(UNAME_S)),MINGW)
+   ECHO_MESSAGE = "Windows"
+   LIBS = -lgdi32 -lopengl32 -limm32 `pkg-config --static --libs sdl2`
+
+   CXXFLAGS = -Iimgui -Iimgui/examples/libs/gl3w `pkg-config --cflags sdl2`
+   CXXFLAGS += -Wall -Wformat
+   CFLAGS = $(CXXFLAGS)
+endif
+
+
+.cpp.o:
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+all: $(EXE)
+	@echo Build complete for $(ECHO_MESSAGE)
+
+$(EXE): $(OBJS)
+	$(CXX) -o $(EXE) $(OBJS) $(CXXFLAGS) $(LIBS)
+
+clean:
+	rm $(EXE) $(OBJS)
--- main.cpp
+++ main.cpp
@@ -1,193 +1,140 @@
-// Boost.Polygon library voronoi_basic_tutorial.cpp file
+// ImGui - standalone example application for SDL2 + OpenGL
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
 
-//          Copyright Andrii Sydorchuk 2010-2012.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
+#include <imgui.h>
+#include "imgui_impl_sdl_gl3.h"
+#include <stdio.h>
+#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
+#include <SDL.h>
 
-// See http://www.boost.org for updates, documentation, and revision history.
+#include "voronoi_main.hpp"
 
-#include <cstdio>
-#include <vector>
-
-#include <boost/polygon/voronoi.hpp>
-using boost::polygon::voronoi_builder;
-using boost::polygon::voronoi_diagram;
-using boost::polygon::x;
-using boost::polygon::y;
-using boost::polygon::low;
-using boost::polygon::high;
-
-#include "voronoi_visual_utils.hpp"
-
-struct Point {
-  int a;
-  int b;
-  Point(int x, int y) : a(x), b(y) {}
-};
-
-struct Segment {
-  Point p0;
-  Point p1;
-  Segment(int x1, int y1, int x2, int y2) : p0(x1, y1), p1(x2, y2) {}
-};
-
-namespace boost {
-namespace polygon {
-
-template <>
-struct geometry_concept<Point> {
-  typedef point_concept type;
-};
-
-template <>
-struct point_traits<Point> {
-  typedef int coordinate_type;
-
-  static inline coordinate_type get(
-      const Point& point, orientation_2d orient) {
-    return (orient == HORIZONTAL) ? point.a : point.b;
-  }
-};
-
-template <>
-struct geometry_concept<Segment> {
-  typedef segment_concept type;
-};
-
-template <>
-struct segment_traits<Segment> {
-  typedef int coordinate_type;
-  typedef Point point_type;
-
-  static inline point_type get(const Segment& segment, direction_1d dir) {
-    return dir.to_int() ? segment.p1 : segment.p0;
-  }
-};
-}  // polygon
-}  // boost
-
-// Traversing Voronoi edges using edge iterator.
-int iterate_primary_edges1(const voronoi_diagram<double>& vd) {
-  int result = 0;
-  for (voronoi_diagram<double>::const_edge_iterator it = vd.edges().begin();
-       it != vd.edges().end(); ++it) {
-    if (it->is_primary())
-      ++result;
-  }
-  return result;
-}
-
-// Traversing Voronoi edges using cell iterator.
-int iterate_primary_edges2(const voronoi_diagram<double> &vd) {
-  int result = 0;
-  for (voronoi_diagram<double>::const_cell_iterator it = vd.cells().begin();
-       it != vd.cells().end(); ++it) {
-    const voronoi_diagram<double>::cell_type& cell = *it;
-    const voronoi_diagram<double>::edge_type* edge = cell.incident_edge();
-    // This is convenient way to iterate edges around Voronoi cell.
-    do {
-      if (edge->is_primary())
-        ++result;
-      edge = edge->next();
-    } while (edge != cell.incident_edge());
-  }
-  return result;
-}
-
-// Traversing Voronoi edges using vertex iterator.
-// As opposite to the above two functions this one will not iterate through
-// edges without finite endpoints and will iterate only once through edges
-// with single finite endpoint.
-int iterate_primary_edges3(const voronoi_diagram<double> &vd) {
-  int result = 0;
-  for (voronoi_diagram<double>::const_vertex_iterator it =
-       vd.vertices().begin(); it != vd.vertices().end(); ++it) {
-    const voronoi_diagram<double>::vertex_type& vertex = *it;
-    const voronoi_diagram<double>::edge_type* edge = vertex.incident_edge();
-    // This is convenient way to iterate edges around Voronoi vertex.
-    do {
-      if (edge->is_primary())
-        ++result;
-      edge = edge->rot_next();
-    } while (edge != vertex.incident_edge());
-  }
-  return result;
-}
-
-int main() {
-  // Preparing Input Geometries.
-  std::vector<Point> points;
-  points.push_back(Point(0, 0));
-  points.push_back(Point(1, 6));
-  std::vector<Segment> segments;
-  segments.push_back(Segment(-4, 5, 5, -1));
-  segments.push_back(Segment(3, -11, 13, -1));
-
-  // Construction of the Voronoi Diagram.
-  voronoi_diagram<double> vd;
-  construct_voronoi(points.begin(), points.end(),
-                    segments.begin(), segments.end(),
-                    &vd);
-
-  // Traversing Voronoi Graph.
-  {
-    printf("Traversing Voronoi graph.\n");
-    printf("Number of visited primary edges using edge iterator: %d\n",
-        iterate_primary_edges1(vd));
-    printf("Number of visited primary edges using cell iterator: %d\n",
-        iterate_primary_edges2(vd));
-    printf("Number of visited primary edges using vertex iterator: %d\n",
-        iterate_primary_edges3(vd));
-    printf("\n");
-  }
-
-  // Using color member of the Voronoi primitives to store the average number
-  // of edges around each cell (including secondary edges).
-  {
-    printf("Number of edges (including secondary) around the Voronoi cells:\n");
-    for (voronoi_diagram<double>::const_edge_iterator it = vd.edges().begin();
-         it != vd.edges().end(); ++it) {
-      std::size_t cnt = it->cell()->color();
-      it->cell()->color(cnt + 1);
+int main(int, char**)
+{
+    // Setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
+    {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
     }
+
+    // Setup window
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_DisplayMode current;
+    SDL_GetCurrentDisplayMode(0, &current);
+    SDL_Window *window = SDL_CreateWindow("ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+    gl3wInit();
+
+    // Setup ImGui binding
+    ImGui_ImplSdlGL3_Init(window);
+
+    // Load Fonts
+    // (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
+    // ImGuiIO& io = ImGui::GetIO();
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
+    // io.Fonts->AddFontFromFileTTF("imgui/extra_fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
+    // io.Fonts->AddFontFromFileTTF("imgui/extra_fonts/ProggyTiny.ttf", 10.0f);
+    // io.Fonts->AddFontFromFileTTF("imgui/extra_fonts/DroidSans.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+
+    bool show_test_window = false;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(114, 144, 154);
+
+    std::vector<Point> points;
+    points.push_back(Point(0, 0));
+    points.push_back(Point(1, 6));
+    std::vector<Segment> segments;
+    segments.push_back(Segment(-4, 5, 5, -1));
+    segments.push_back(Segment(3, -11, 13, -1));
+
+    voronoi_diagram<double> vd;
+    construct_voronoi(points.begin(), points.end(),
+                      segments.begin(), segments.end(),
+                      &vd);
+
     for (voronoi_diagram<double>::const_cell_iterator it = vd.cells().begin();
          it != vd.cells().end(); ++it) {
-      printf("%lu ", it->color());
-    }
-    printf("\n");
-    printf("\n");
-  }
-
-  // Linking Voronoi cells with input geometries.
-  {
-    unsigned int cell_index = 0;
-    for (voronoi_diagram<double>::const_cell_iterator it = vd.cells().begin();
-         it != vd.cells().end(); ++it) {
-      if (it->contains_point()) {
-        std::size_t index = it->source_index();
-        Point p = points[index];
-        printf("Cell #%ud contains a point: (%d, %d).\n",
-               cell_index, x(p), y(p));
-      } else {
-        std::size_t index = it->source_index() - points.size();
-        Point p0 = low(segments[index]);
-        Point p1 = high(segments[index]);
-        if (it->source_category() ==
-            boost::polygon::SOURCE_CATEGORY_SEGMENT_START_POINT) {
-          printf("Cell #%ud contains segment start point: (%d, %d).\n",
-                 cell_index, x(p0), y(p0));
-        } else if (it->source_category() ==
-                   boost::polygon::SOURCE_CATEGORY_SEGMENT_END_POINT) {
-          printf("Cell #%ud contains segment end point: (%d, %d).\n",
-                 cell_index, x(p0), y(p0));
-        } else {
-          printf("Cell #%ud contains a segment: ((%d, %d), (%d, %d)). \n",
-                 cell_index, x(p0), y(p0), x(p1), y(p1));
+      const voronoi_diagram<double>::cell_type& cell = *it;
+      const voronoi_diagram<double>::edge_type* edge = cell.incident_edge();
+      // This is convenient way to iterate edges around Voronoi cell.
+      do {
+        if (edge->is_primary()) {
+          printf("edge\n");
+          // printf("%f %f\n", edge->vertex0()->x(), edge->vertex0()->y());
+          // printf("%f %f\n\n", edge->vertex1()->x(), edge->vertex1()->y());
         }
-      }
-      ++cell_index;
+        edge = edge->next();
+      } while (edge != cell.incident_edge());
     }
-  }
-  return 0;
+
+    // Main loop
+    bool done = false;
+    while (!done)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSdlGL3_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                done = true;
+        }
+        ImGui_ImplSdlGL3_NewFrame(window);
+
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        // if (show_test_window)
+        // {
+        //     ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
+        //     ImGui::ShowTestWindow(&show_test_window);
+        // }
+
+        // Rendering
+        glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+        ImGui::Render();
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    ImGui_ImplSdlGL3_Shutdown();
+    SDL_GL_DeleteContext(glcontext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
