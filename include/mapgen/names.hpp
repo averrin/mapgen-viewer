1be8b44885f74c83ef7d0bb0e3b54e7935a5d6e6
Alexey Nabrodov <alexey.nabrodov@team.wrike.com>
2017-09-23 01:43:18 +0300

    still broken

---
+++ include/mapgen/names.hpp
@@ -0,0 +1,10 @@
+#ifndef NAMES_HPP_
+#define NAMES_HPP_
+
+namespace names {
+  std::string generateRiverName(std::mt19937 gen);
+  std::string generateLandName(std::mt19937 gen);
+  std::string generateSeaName(std::mt19937 gen);
+  std::string generateCityName(std::mt19937 gen);
+};
+#endif
